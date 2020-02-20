09dbfe8bd8605d0e94a75afe8ba950618180ef1f
lucascvarani <42304402+lucascvarani@users.noreply.github.com>
2020-02-20 20:52:27 -0300

    application-teste

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login'
 
 function App() {
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookLogin
+    appId="806019639896279"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={console.log('clicou')}
+    callback={responseFacebook} />
     </div>
   );
 }
