5af4ddf8977e0eb7b3f9b0b6b470b6fe3f35b3bc
lucascvarani <42304402+lucascvarani@users.noreply.github.com>
2020-02-20 21:15:47 -0300

    build

---
+++ build/precache-manifest.6ff041ec94a07bbcd42bc62aafbeab3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e571f0735711fc0d055b2171c0d852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c2709733fde6787cb7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "55c10d8c3492d03a258d",
+    "url": "/static/js/2.e70a0ffe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e70a0ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c2709733fde6787cb7",
+    "url": "/static/js/main.186f1ec4.chunk.js"
+  },
+  {
+    "revision": "76b28652aeec71ed0223",
+    "url": "/static/js/runtime-main.3aaf24a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
